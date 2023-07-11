44c59538dac96ca45bfbd09e6500354df2838e7c
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-07-11 11:07:00 +0430

    student

---
+++ consolestudent/consolestudent/mark.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolestudent
+{
+    class mark:student
+    {
+        public  void  maark()
+        {
+           
+            Console.WriteLine(20);
+        }
+        
+        
+    }
+}
---
+++ consolestudent/consolestudent/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolestudent
+{
+    class student
+    {
+        public int cod=2020;
+        public string lname="amir";
+        public string fname="alilu";
+        public int date=1380;
+    }
+   
+}
